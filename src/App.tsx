f0c43b66cbe7ac10182288eba8d87570560f84eb
Dave <davefunk135@gmail.com>
2021-01-26 21:33:34 -0800

    testing cors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const url = 'https://overwatch-companion.herokuapp.com/api/users';
+    const req = fetch(url).then(data => data.json());
+
+    req.then(data => setData(data));
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +19,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {data.map((data: any) => {
+          return (
+            <div key={data.id}>
+              <p>{data.id}</p>
+              <p>{data.battletag}</p>
+              <p>{data.identifier}</p>
+            </div>
+          )
+        })}
         <a
           className="App-link"
           href="https://reactjs.org"
